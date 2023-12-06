dfdef4768463f1870ce6de81ab7fb23343ba040f
Rômulo Souza Ribeiro <ART0044580@aluno.firjansenaisesi.com.br>
2023-12-06 09:30:14 -0300

    controller API

--- unico/src/main/java/com/banco/unico/controller/ClienteController.java
+++ unico/src/main/java/com/banco/unico/controller/ClienteController.java
@@ -1,13 +1,78 @@
 package com.banco.unico.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.unico.entities.Cliente;
+import com.banco.unico.service.ClienteService;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
     
-    // @Autowired
+    @Autowired
+    private ClienteService clienteService;
 
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAllClientes(){
+        
+        List<Cliente> clientes = clienteService.getAllClientes();
+
+        return ResponseEntity.ok(clientes);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClientesById(@PathVariable Long id){
+        
+        Cliente cliente = clienteService.getClienteById(id);
+
+        if (cliente != null){
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(cliente, HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @PostMapping
+    public ResponseEntity<Cliente> saveCliente(@RequestBody Cliente novoCliente){
+
+        Cliente cliente = clienteService.saveCliente(novoCliente);
+        return ResponseEntity.ok(cliente);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente (@RequestBody Cliente clienteAtualizado, @PathVariable Long id) {
+
+        Cliente cliente = clienteService.updateCliente(id, clienteAtualizado);
+        return ResponseEntity.ok(cliente);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> deleteCliente(@PathVariable Long id){
+
+        Cliente cliente = clienteService.deleteCliente(id);
+        
+        return ResponseEntity.ok(cliente);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Cliente> logicalDeleteCliente(@PathVariable Long id){
+
+        Cliente cliente = clienteService.getClienteById(id);
+        
+        if (cliente != null){
+            clienteService.logicalDeleteCliente(id);
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(cliente, HttpStatus.NOT_FOUND);
+        }
+    }
 }
